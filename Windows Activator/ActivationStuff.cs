c9800a8c889e964e27bc541e25773b354f037b72
Muhammad Fatah <ab.fatahmn@hotmail.com>
2019-12-25 13:32:09 +0600

    removed useless comments

--- Windows Activator/ActivationStuff.cs
+++ Windows Activator/ActivationStuff.cs
@@ -12,30 +12,6 @@ namespace Windows_Activator
     {
         public ActivationStuff()
         {
-
-           /* var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
-            processInfo.CreateNoWindow = true;
-            processInfo.UseShellExecute = false;
-            processInfo.RedirectStandardError = true;
-            processInfo.RedirectStandardOutput = true;
-
-            var process = Process.Start(processInfo);
-
-            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
-                //Console.WriteLine("output>>" + e.Data);
-                //MessageBox.Show(e.Data);
-            process.BeginOutputReadLine();
-
-            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
-                //Console.WriteLine("error>>" + e.Data);
-               // MessageBox.Show(e.Data);
-            process.BeginErrorReadLine();
-
-            process.WaitForExit();
-
-            //Console.WriteLine("ExitCode: {0}", process.ExitCode);
-            //MessageBox.Show(process.ExitCode.ToString());
-            process.Close(); */
         }
         public string ActivateMe(string command)
         {
